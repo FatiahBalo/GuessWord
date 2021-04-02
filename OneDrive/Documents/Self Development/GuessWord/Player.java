44413eabdbc2b83f3c71f4c5c2be00ac137eff95
FatiahBalo@github.com <fatiah.balogun@gmail.com>
2021-04-01 21:20:23 -0600

    Added new java class; Player
    This new class holds the player information like player name, and lives

---
+++ OneDrive/Documents/Self Development/GuessWord/Player.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class Player here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Player
+{
+    String name; 
+    int livesLeft;
+    public Player(String newName)
+    {
+        name = newName;
+        livesLeft = 3;
+    }
+    
+    public void loseLife()
+    {
+        livesLeft--;
+    }
+    
+    public int getLivesLeft()
+    {
+        return livesLeft;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+}
